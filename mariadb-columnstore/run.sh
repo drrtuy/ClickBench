e8105d491029e12d54b3954aefea3ac8e400cd13
drrtuy <drrtuy@gmail.com>
2025-07-10 18:11:21 +0000

    MariaDB Columnstore 23.10.4 results

--- mariadb-columnstore/run.sh
+++ mariadb-columnstore/run.sh
@@ -7,6 +7,6 @@ cat queries.sql | while read -r query; do
     echo 3 | sudo tee /proc/sys/vm/drop_caches
 
     for i in $(seq 1 $TRIES); do
-        mysql --password="${PASSWORD}" --host 127.0.0.1 -vvv test -e "${query}"
+        mariadb -vvv test -e "${query}"
     done;
 done;
